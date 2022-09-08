467518f75a1dec1420b51d35233d84bb4f9b0505
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2022-09-08 00:41:17 -0300

    load file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import {wait} from './wait';
+import {promises as fs} from 'fs';
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+
+    const configFileName = core.getInput('config-file');
+    const content = await fs.readFile(configFileName, 'utf8');
+    core.debug(content);
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
